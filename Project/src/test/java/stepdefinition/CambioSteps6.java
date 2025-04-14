85022cf223416600cf0cdcb4675a7021f253040c
shibin-kurian <shibinkurian2001@gmail.com>
2025-04-14 06:22:47 +0000

    initial

--- Project/src/test/java/stepdefinition/CambioSteps6.java
+++ Project/src/test/java/stepdefinition/CambioSteps6.java
@@ -1,4 +1,4 @@
-package stepdefinitions;
+package stepdefinition;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
--- Project/src/test/java/stepdefinition/Hooks.java
+++ Project/src/test/java/stepdefinition/Hooks.java
@@ -1,4 +1,4 @@
-package stepdefinitions;
+package stepdefinition;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
